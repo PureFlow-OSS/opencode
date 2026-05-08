2c43c7ce834f8fe81d8e6619ab777da4bcaced14
Klaus Scheiböck <klaus.scheiboeck@programmierfabrik.at>
2026-05-08 09:50:25 +0200

    Bump version to 1.14.34

--- packages/desktop-electron/updater-helper/obj/Release/net10.0-windows/win-x64/OpenCode.UpdaterHelper.AssemblyInfo.cs
+++ packages/desktop-electron/updater-helper/obj/Release/net10.0-windows/win-x64/OpenCode.UpdaterHelper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenCode.UpdaterHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c397a03eedc0d17a82852059fb6b23de311b7f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab5ad5f42f91ed744e9df5640f3ef23bfc6ddad")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenCode.UpdaterHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenCode.UpdaterHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
